33c32eb244eb2cf9cc9cecec2f207299b814d16d
Alvaro-Kothe <kothe65@gmail.com>
2023-08-01 12:21:52 -0300

    Document plots functions

--- R/diagnostic_plots.R
+++ R/diagnostic_plots.R
@@ -1,26 +1,75 @@
+#' Plot Cook's distances
+#'
+#' @param model Model with [cooks.distance()] method
+#' @param n_highlights Number of points with highest cook distance to highlight in the plot
+#' @param cut Boolean indicating to show horizontal line of 4 standard deviations
+#' above the Cook's distances mean.
+#' @param xlab x-axis label
+#' @param ylab y-axis label
+#'
+#' @return Cook's distances invisible
+#' @export
+#'
+#' @examples
+#' fit <- lm(mpg ~ cyl, data = mtcars)
+#'
+#' plot_cook(fit)
+#' plot_cook(fit, n_highlights = 2)
 plot_cook <- function(model, n_highlights = 0, cut = FALSE,
                       xlab = "Index", ylab = "Cook's distance") {
-  cook <- cooks.distance(model)
+  cook <- stats::cooks.distance(model)
 
   plot(seq_along(cook), cook, pch = 20, xlab = xlab, ylab = ylab)
   if (n_highlights > 0) {
     highests <- order(cook, decreasing = TRUE)[seq_len(n_highlights)]
-    text(x = highests, y = cook[highests], label = highests)
+    graphics::text(x = highests, y = cook[highests], label = highests)
   }
   if (cut) {
-    abline(h = mean(cook) + 4 * sd(cook))
+    graphics::abline(h = base::mean(cook) + 4 * stats::sd(cook))
   }
+
+  return(invisible(cook))
 }
 
-plot_res_vs_linear_predictor <- function(model, residual_fn = rstandard,
-                                         type = "deviance",
+#' Plot Residuals against Linear Predictor
+#'
+#' @param model Model with methods [predict()] or [fitted()]
+#' @param residual_fn function to compute residuals, default [rstandard()],
+#' other functions could be [rstudent()], [residuals()].
+#' @param xlab x-axis label
+#' @param ylab y-axis label
+#' @param ... Extra arguments to `residual_fn`
+#'
+#' @return invisible list with plot components
+#'
+#' @details
+#' If the model was fitted using the [glm()] function, it will use the [predict()]
+#' method, otherwise, it will use the [fitted()] method.
+#'
+#' @export
+#'
+#' @examples
+#' fit <- lm(mpg ~ cyl, data = mtcars)
+#'
+#' plot_res_vs_linear_predictor(fit)
+#' plot_res_vs_linear_predictor(fit, residual_fn = rstudent)
+#' plot_res_vs_linear_predictor(fit, residual_fn = residuals)
+#'
+#'
+#' glm_fit <- glm(cyl ~ mpg, family = poisson(), data = mtcars)
+#'
+#' plot_res_vs_linear_predictor(glm_fit)
+#' plot_res_vs_linear_predictor(glm_fit, type = "pearson")
+plot_res_vs_linear_predictor <- function(model, residual_fn = stats::rstandard,
                                          xlab = "Linear Predictor",
                                          ylab = "Standardized deviance residuals",
-                                         main = "") {
-  linear_predictor <- switch(class(model),
-    glm = predict(model, type = "link"),
-    fitted(model)
+                                         ...) {
+  linear_predictor <- switch(class(model)[1],
+    glm = stats::predict(model, type = "link"),
+    stats::fitted(model)
   )
+  y <- residual_fn(model, ...)
+  plot(x = linear_predictor, y, xlab = xlab, ylab = ylab)
 
-  plot(x = linear_predictor, residual_fn(model))
+  return(invisible(list(x = linear_predictor, y = y)))
 }
