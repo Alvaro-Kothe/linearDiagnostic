fd3b0092bc34babfa6eba9313301c419c1ddabc8
Alvaro-Kothe <kothe65@gmail.com>
2023-09-09 16:57:57 -0300

    Add documentation to `backward_selection`

--- R/selection.R
+++ R/selection.R
@@ -1,5 +1,10 @@
 remove_variable <- function(x) {
+
+  INTERCEPT = "(Intercept)"
+  remove_intercept <- INTERCEPT %in% x
+
   rhs <- paste0(x, collapse = "-")
+  if (remove_intercept) rhs <- paste(rhs, "+ 0")
   out <- stats::as.formula(paste0(". ~ . - ", rhs))
 
   return(out)
@@ -16,6 +21,26 @@ get_p_value <- function(model) {
   return(summary(model)[["coefficients"]][, 4])
 }
 
+#' Select covariates backwise
+#'
+#' @param model A model with [stats::update()] method.
+#' @param threshold Value threshold to remove variable. where the variable is
+#' removed if `measure_fn(model) >= threshold`.
+#' @param measure_fn Function with model as argument and returns values to be used by
+#' `threshold`. Default is p-value reported by [summary()].
+#' @param data Data to be used for model refit.
+#' @param max_steps Maximum number of steps for selection.
+#' @param return_step_results Boolean that if `TRUE` returns a list with the first
+#' argument the selection result, and second argument a named vector with
+#' `measure_fn` measures and coefficient removed.
+#' @param do_not_remove String vector of variables to not be removed in the selection.
+#'
+#' @return the selected model if `return_step_results` is `False`.
+#' @export
+#'
+#' @examples
+#' model <- lm(mpg ~ ., data = mtcars)
+#' backward_selection(model)
 backward_selection <- function(model, threshold = .15,
                                measure_fn = get_p_value,
                                data = stats::model.frame(model),
