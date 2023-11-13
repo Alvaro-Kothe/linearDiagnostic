bf1e68a802f19fa146864960fc5bd1ee567008f4
Alvaro-Kothe <kothe65@gmail.com>
2023-11-13 13:06:25 -0300

    Move sample size plots tests

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -3,11 +3,6 @@ test_that("Plot functions runs without problems", {
   y <- c(2, 3, 6, 9)
   fit <- lm(y ~ x)
 
-  # Don't show any plot
-  expect_no_error(plot_joint_pvalues_ecdf(fit))
-  expect_no_error(plot_pvalues_ecdf(fit))
-  expect_no_error(plot_pvalues_ecdf(fit, ask = TRUE))
-  expect_no_error(plot_pvalues_ecdf(fit, ask = FALSE))
   expect_no_error(plot_cook(fit))
   expect_no_error(plot_cook(fit, n_highlights = length(x) + 1))
   expect_no_error(plot_cook(fit, n_highlights = 2))
