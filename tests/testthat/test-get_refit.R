6feb9e3ac6738537b5fc30abd9a8b2b2b2d562e8
Alvaro-Kothe <kothe65@gmail.com>
2024-04-13 12:23:48 -0300

    Rename utils.R to get_refit.R and fix get_refit
    Fix bug with get_refit when models are adjusted using c(...) on the
    formula's lhs.

---
+++ tests/testthat/test-get_refit.R
@@ -0,0 +1,12 @@
+test_that("response is replaced when using c() as response", {
+  fit <- lm(c(2, 4, 5) ~ c(3, 5, 7))
+  model_refit <- get_refit(fit, c(1, 1, 1))
+  expect_equal(stats::model.frame(model_refit)[, 1], c(1, 1, 1))
+})
+
+test_that("response is replaced when using data", {
+  df <- data.frame(y = c(2, 4, 5), x = c(1, 2, 3))
+  fit <- lm(y ~ x, data = df)
+  model_refit <- get_refit(fit, c(1, 1, 1))
+  expect_equal(stats::model.frame(model_refit)[[1]], c(1, 1, 1))
+})
